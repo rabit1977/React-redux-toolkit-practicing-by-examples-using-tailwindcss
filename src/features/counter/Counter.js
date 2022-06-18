8e103f5af0e46e2a859c9ab270071ba09e7ab665
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-06-18 23:53:19 +0200

    redux-toolkit quick start by example

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
+  increment, incrementAsync, incrementByAmount, incrementIfOdd,
+  selectCount
 } from './counterSlice';
-import styles from './Counter.module.css';
 
 export function Counter() {
   const count = useSelector(selectCount);
@@ -19,44 +15,44 @@ export function Counter() {
 
   return (
     <div>
-      <div className={styles.row}>
+      <div className='flex items-center justify-center mb-4'>
         <button
-          className={styles.button}
-          aria-label="Decrement value"
+          className='appearance-none bg-none text-4xl px-4 text-[#704cb6] pb-1 rounded-sm outline-none transition-all delay-75 cursor-pointer border-2 border-solid border-transparent bg-[#704cb61a] ml-1 mr-2 hover:border-[#704cb666] hover:border-2 hover:border-solid focus:border-[#704cb666] focus:border-2 focus:border-solid active:bg-[#704cb633]'
+          aria-label='Decrement value'
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
-        <span className={styles.value}>{count}</span>
+        <span className='text-7xl ml-4 mr-5'>{count}</span>
         <button
-          className={styles.button}
-          aria-label="Increment value"
+          className='appearance-none bg-none text-4xl px-3 text-[#704cb6] pb-1 rounded-sm outline-none transition-all delay-75 cursor-pointer border-2 border-solid border-transparent bg-[#704cb61a] ml-1 mr-2 hover:border-[#704cb666] hover:border-2 hover:border-solid focus:border-[#704cb666] focus:border-2 focus:border-solid active:bg-[#704cb633]'
+          aria-label='Increment value'
           onClick={() => dispatch(increment())}
         >
           +
         </button>
       </div>
-      <div className={styles.row}>
+      <div className='flex items-center justify-center last:mb-3'>
         <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
+          className='mr-1 text-2xl text-center w-16 p-0.5'
+          aria-label='Set increment amount'
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
-          className={styles.button}
+          className='appearance-none bg-none text-4xl px-3 text-[#704cb6] pb-1 rounded-sm outline-none transition-all delay-75 cursor-pointer border-2 border-solid border-transparent bg-[#704cb61a] ml-1 mr-2 hover:border-[#704cb666] hover:border-2 hover:border-solid focus:border-[#704cb666] focus:border-2 focus:border-solid active:bg-[#704cb633]'
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
-          className={styles.asyncButton}
+          className='appearance-none bg-none text-4xl px-3 text-[#704cb6] pb-1 rounded-sm outline-none transition-all delay-75 cursor-pointer border-2 border-solid border-transparent bg-[#704cb61a] ml-1 mr-2 hover:border-[#704cb666] hover:border-2 hover:border-solid focus:border-[#704cb666] focus:border-2 focus:border-solid active:bg-[#704cb633]'
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
-          className={styles.button}
+          className='appearance-none bg-none text-4xl px-3 text-[#704cb6] pb-1 rounded-sm outline-none transition-all delay-75 cursor-pointer border-2 border-solid border-transparent bg-[#704cb61a] ml-1 mr-2 hover:border-[#704cb666] hover:border-2 hover:border-solid focus:border-[#704cb666] focus:border-2 focus:border-solid active:bg-[#704cb633]'
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
           Add If Odd
